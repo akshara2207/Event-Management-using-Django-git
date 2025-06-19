dcfab36a11baa7e0a1fa512a40e6ada81e45d6d1
akshara2207 <aksharasambasivan007@gmail.com>
2025-06-19 22:30:14 +0530

    first commit

--- eventapp/views.py
+++ eventapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .models import Event
 # Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+def about(request):
+    return render(request,"about.html")
+
+def booking(request):
+    return render(request,"booking.html")
+
+def events(request):
+    dict_eve={
+        'eve':Event.objects.all()
+    }
+    return render(request,"events.html",dict_eve)
+
+def contact(request):
+    return render(request,"contact.html")
